6f07259ec5e4d04db286763a86eeb911e344100b
suyang <suyang@haohandata.com.cn>
2021-08-17 10:12:20 +0800

    journal分页查询

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,20 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
+val vers = rootProject.extra.get("vers") as Map<String, String>
+
+plugins {
+    id("kotlin-kapt")
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.2")
     implementation("org.postgresql:postgresql")
+
+    api("com.querydsl:querydsl-jpa:${vers["queryDSL"]}")
+    kapt("com.querydsl:querydsl-apt:${vers["queryDSL"]}:jpa")
 }
 
 val bootJar: BootJar by tasks
--- app/src/main/kotlin/com/github/xsi640/Application.kt
+++ app/src/main/kotlin/com/github/xsi640/Application.kt
@@ -1,8 +1,12 @@
 package com.github.xsi640
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
+import javax.persistence.EntityManager
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,4 +14,12 @@ class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
+}
+
+@Configuration
+class RepositoryConfiguration {
+    @Bean
+    fun jpaQueryFactory(entityManager: EntityManager): JPAQueryFactory {
+        return JPAQueryFactory(entityManager)
+    }
 }
